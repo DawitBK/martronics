f3159222c09884c1f5b451da066af209c1e590f4
Dave Bayou <dawitbk01@gmail.com>
2025-11-26 18:24:54 +0300

    cors enabled

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://martronics.vercel.app/api",
+    "https://martronics.vercel.app",
     "https://web-production-cfbdd.up.railway.app/api" // Add your Vercel frontend domain here
   ],
   credentials: true
